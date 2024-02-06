75fe58435081fa5ec6690fd2e4ba3afc8d449a26
Jim Durand <me@jdurand.com>
2024-02-05 23:10:52 -0500

    nvim bufferline: use slant tabs

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -7,7 +7,7 @@ return {
       vim.opt.termguicolors = true
       require("bufferline").setup({
         options = {
-          separator_style = 'thick', -- "slant" | "slope" | "thick" | "thin" | { 'any', 'any' },
+          separator_style = 'slant', -- "slant" | "slope" | "thick" | "thin" | { 'any', 'any' },
           sort_by = 'relative_directory', -- 'insert_after_current' |'insert_at_end' | 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
         }
       })

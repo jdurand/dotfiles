d7ba2a6f5ae5d1ca8abe4607441829bad3b66ce4
Jim Durand <me@jdurand.com>
2024-02-06 15:53:00 -0500

    nvim: update chat GPT plugin

--- nvim/lua/plugins/chat-gpt.lua
+++ nvim/lua/plugins/chat-gpt.lua
@@ -1,5 +1,18 @@
 return {
   {
+    'jackMort/ChatGPT.nvim',
+    event = 'VeryLazy',
+    config = function()
+      require('chatgpt').setup()
+    end,
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'nvim-lua/plenary.nvim',
+      'folke/trouble.nvim',
+      'nvim-telescope/telescope.nvim'
+    }
+  },
+  {
     "dpayne/CodeGPT.nvim",
     dependencies = {
       'nvim-lua/plenary.nvim',
@@ -22,5 +35,5 @@ return {
         }
       }
     end
-  },
+  }
 }

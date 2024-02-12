de4deb8df21c51b8edad384f967dfe6e986c9294
Jim Durand <me@jdurand.com>
2024-02-11 23:42:07 -0500

    nvim: cleanup plugins

--- nvim/lua/plugins/todo.lua
+++ nvim/lua/plugins/todo.lua
@@ -1,29 +1,24 @@
 return {
-  'rstacruz/vim-closer',
-  'mileszs/ack.vim',
-  'vim-scripts/camelcasemotion',
-  'tmhedberg/matchit',
   'tpope/vim-abolish',
-  'vim-scripts/YankRing.vim',
-  'ervandew/supertab',
-  'jszakmeister/vim-togglecursor',
-  'embear/vim-localvimrc',
-  'fvictorio/vim-extract-variable',
-  'w0rp/ale',
-  'christoomey/vim-tmux-navigator',
   'tpope/vim-eunuch',
-  'nvim-lua/plenary.nvim',
+  'mileszs/ack.vim',
+  'voldikss/vim-floaterm',
+  'christoomey/vim-tmux-navigator',
   'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+  'nvim-lua/plenary.nvim',
   'MunifTanjim/nui.nvim',
   'wincent/terminus',
-  'voldikss/vim-floaterm',
   'RRethy/vim-illuminate',
-  -- 'sheerun/vim-polyglot',
+  'w0rp/ale',
+  'vim-scripts/YankRing.vim',
+  'ervandew/supertab',
+  'fvictorio/vim-extract-variable',
+  -- {
+  --   'prettier/vim-prettier',
+  --   build = 'yarn install',
+  -- },
   {
     'jalvesaq/Nvim-R',
     branch = 'stable',
   },
 }
-
--- 'prettier/vim-prettier', { 'do': 'yarn install' },
--- " Plug 'prettier/vim-prettier', {

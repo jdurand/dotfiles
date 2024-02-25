897738bb01316a7701240d5ac252c56ff4d54b9c
Jim Durand <me@jdurand.com>
2024-02-25 16:47:20 -0500

    nvim: adjust bufferline config

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -7,8 +7,8 @@ return {
       vim.opt.termguicolors = true
       require("bufferline").setup({
         options = {
-          separator_style = 'slant', -- "slant" | "slope" | "thick" | "thin" | { 'any', 'any' },
-          sort_by = 'relative_directory', -- 'insert_after_current' |'insert_at_end' | 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
+          diagnostics = 'nvim_lsp',
+          separator_style = 'thick', -- "slant" | "slope" | "thick" | "thin" | { 'any', 'any' },
         }
       })
     end,

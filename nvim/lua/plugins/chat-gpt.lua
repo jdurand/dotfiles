f48d870ce3cebbb0f972240e31d231e40bc88008
Jim Durand <me@jdurand.com>
2024-02-25 16:50:32 -0500

    nvim: chat gpt use default engine (gpt-3.5-turbo)

--- nvim/lua/plugins/chat-gpt.lua
+++ nvim/lua/plugins/chat-gpt.lua
@@ -5,7 +5,11 @@ return {
     config = function()
       local chatgpt = require('chatgpt')
 
-      chatgpt.setup()
+      chatgpt.setup({
+        openai_params = {
+          -- model = "gpt-4-turbo-preview",
+        },
+      })
 
       vim.keymap.set('n', '<leader>gt', chatgpt.openChat, {})
       vim.keymap.set('n', '<leader>gp', chatgpt.selectAwesomePrompt, {})
@@ -18,28 +22,28 @@ return {
       'nvim-telescope/telescope.nvim'
     }
   },
-  {
-    "dpayne/CodeGPT.nvim",
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'MunifTanjim/nui.nvim',
-    },
-    config = function()
-      require("codegpt.config")
+  -- {
+  --   "dpayne/CodeGPT.nvim",
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --     'MunifTanjim/nui.nvim',
+  --   },
+  --   config = function()
+  --     require("codegpt.config")
 
-      vim.g["codegpt_commands"] = {
-        ["modernize"] = {
-          -- model = "gpt-3.5-turbo",
-          model = "gpt-3.5-turbo-16k",
-          -- model = "gpt-4-turbo-preview",
-          -- max_tokens = 4096,
-          max_tokens = 16384,
-          user_message_template = "I have the following {{language}} code: ```{{filetype}}\n{{text_selection}}```\nModernize the above code. Use current best practices. Only return the code snippet and comments. {{language_instructions}}",
-          language_instructions = {
-            cpp = "Refactor the code to use trailing return type, and the auto keyword where applicable.",
-          },
-        }
-      }
-    end
-  }
+  --     vim.g["codegpt_commands"] = {
+  --       ["modernize"] = {
+  --         -- model = "gpt-3.5-turbo",
+  --         model = "gpt-3.5-turbo-16k",
+  --         -- model = "gpt-4-turbo-preview",
+  --         -- max_tokens = 4096,
+  --         max_tokens = 16384,
+  --         user_message_template = "I have the following {{language}} code: ```{{filetype}}\n{{text_selection}}```\nModernize the above code. Use current best practices. Only return the code snippet and comments. {{language_instructions}}",
+  --         language_instructions = {
+  --           cpp = "Refactor the code to use trailing return type, and the auto keyword where applicable.",
+  --         },
+  --       }
+  --     }
+  --   end
+  -- }
 }

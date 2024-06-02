d067158c56c26ab340d65b401f5f3006995787eb
Jim Durand <me@jdurand.com>
2024-06-02 10:04:42 -0400

    neovim: update ChatGPT config

--- nvim/lua/plugins/chat-gpt.lua
+++ nvim/lua/plugins/chat-gpt.lua
@@ -1,31 +1,67 @@
 return {
   {
-    'jackMort/ChatGPT.nvim',
-    event = 'VeryLazy',
-    config = function()
-      local chatgpt = require('chatgpt')
-
-      chatgpt.setup({
-        openai_params = {
-          model = "gpt-4-turbo-preview",
-        },
-      })
-
-      vim.keymap.set('n', '<leader>gt', chatgpt.openChat, {})
-      vim.keymap.set('n', '<leader>gp', chatgpt.selectAwesomePrompt, {})
-      vim.keymap.set('v', '<leader>ge', chatgpt.edit_with_instructions, {})
-    end,
-    dependencies = {
-      'MunifTanjim/nui.nvim',
-      'nvim-lua/plenary.nvim',
-      'folke/trouble.nvim',
-      'nvim-telescope/telescope.nvim'
-    }
-  },
-  {
     'robitx/gp.nvim',
     config = function()
-      require('gp').setup()
+      require('gp').setup({
+        agents = {
+          {
+            name = "ChatGPT4",
+            chat = true,
+            command = false,
+            -- string with model name or table with model name and parameters
+            model = { model = "gpt-4o", temperature = 1.1, top_p = 1 },
+            -- system prompt (use this to specify the persona/role of the AI)
+            system_prompt = "You are a general AI assistant.\n\n"
+              .. "The user provided the additional info about how they would like you to respond:\n\n"
+              .. "- If you're unsure don't guess and say you don't know instead.\n"
+              .. "- Ask question if you need clarification to provide better answer.\n"
+              .. "- Think deeply and carefully from first principles step by step.\n"
+              .. "- Zoom out first to see the big picture and then zoom in to details.\n"
+              .. "- Use Socratic method to improve your thinking and coding skills.\n"
+              .. "- Don't elide any code from your output if the answer requires coding.\n"
+              .. "- Take a deep breath; You've got this!\n",
+          },
+          {
+            name = "ChatGPT3-5",
+            chat = true,
+            command = false,
+            -- string with model name or table with model name and parameters
+            model = { model = "gpt-3.5-turbo", temperature = 1.1, top_p = 1 },
+            -- system prompt (use this to specify the persona/role of the AI)
+            system_prompt = "You are a general AI assistant.\n\n"
+              .. "The user provided the additional info about how they would like you to respond:\n\n"
+              .. "- If you're unsure don't guess and say you don't know instead.\n"
+              .. "- Ask question if you need clarification to provide better answer.\n"
+              .. "- Think deeply and carefully from first principles step by step.\n"
+              .. "- Zoom out first to see the big picture and then zoom in to details.\n"
+              .. "- Use Socratic method to improve your thinking and coding skills.\n"
+              .. "- Don't elide any code from your output if the answer requires coding.\n"
+              .. "- Take a deep breath; You've got this!\n",
+          },
+          {
+            name = "CodeGPT4",
+            chat = false,
+            command = true,
+            -- string with model name or table with model name and parameters
+            model = { model = "gpt-4o", temperature = 0.8, top_p = 1 },
+            -- system prompt (use this to specify the persona/role of the AI)
+            system_prompt = "You are an AI working as a code editor.\n\n"
+              .. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
+              .. "START AND END YOUR ANSWER WITH:\n\n```",
+          },
+          {
+            name = "CodeGPT3-5",
+            chat = false,
+            command = true,
+            -- string with model name or table with model name and parameters
+            model = { model = "gpt-3.5-turbo", temperature = 0.8, top_p = 1 },
+            -- system prompt (use this to specify the persona/role of the AI)
+            system_prompt = "You are an AI working as a code editor.\n\n"
+              .. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
+              .. "START AND END YOUR ANSWER WITH:\n\n```",
+          },
+        },
+      })
 
       require("which-key").register({
         ["<C-g>"] = {
@@ -182,7 +218,32 @@ return {
         nowait = true,
       })
     end,
-  }
+  },
+  --
+  -- {
+  --   'jackMort/ChatGPT.nvim',
+  --   event = 'VeryLazy',
+  --   config = function()
+  --     local chatgpt = require('chatgpt')
+
+  --     -- chatgpt.setup({
+  --     --   openai_params = {
+  --     --     model = "gpt-4-turbo-preview",
+  --     --   },
+  --     -- })
+
+  --     vim.keymap.set('n', '<leader>gt', chatgpt.openChat, {})
+  --     vim.keymap.set('n', '<leader>gp', chatgpt.selectAwesomePrompt, {})
+  --     vim.keymap.set('v', '<leader>ge', chatgpt.edit_with_instructions, {})
+  --   end,
+  --   dependencies = {
+  --     'MunifTanjim/nui.nvim',
+  --     'nvim-lua/plenary.nvim',
+  --     'folke/trouble.nvim',
+  --     'nvim-telescope/telescope.nvim'
+  --   }
+  -- },
+  --
   -- {
   --   "dpayne/CodeGPT.nvim",
   --   dependencies = {

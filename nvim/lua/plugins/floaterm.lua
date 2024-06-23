da9e678c42cf6bca273042d13840c70a59bce941
Jim Durand <me@jdurand.com>
2024-06-23 15:00:16 -0400

    nvim: fix floaterm keymap description

--- nvim/lua/plugins/floaterm.lua
+++ nvim/lua/plugins/floaterm.lua
@@ -4,9 +4,9 @@ return {
   {
     'voldikss/vim-floaterm',
     config = function()
-      nnoremap('<leader>tt', ':FloatermNew<CR>', { desc = 'List [H]arpoon Marks' })
-      nnoremap('<leader>tg', ':FloatermNew lazygit<CR>', { desc = '[A]dd to Harpoon Marks' })
-      nnoremap('<leader>td', ':FloatermNew! --height=0.9 --width=0.95 --wintype=float --name=gtasks --position=bottom gtasks tasks view --tasklist "Reclaim.ai"<CR>', { desc = '[A]dd to Harpoon Marks' })
+      nnoremap('<leader>tt', ':FloatermNew<CR>', { desc = '[T]erminal' })
+      nnoremap('<leader>tg', ':FloatermNew lazygit<CR>', { desc = 'Lazy[G]it' })
+      nnoremap('<leader>td', ':FloatermNew! --height=0.9 --width=0.95 --wintype=float --name=gtasks --position=bottom gtasks tasks view --tasklist "Reclaim.ai"<CR>', { desc = 'Google Tasks (TO[D]O)' })
     end,
   }
 }

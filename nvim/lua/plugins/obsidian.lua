7fd8e5c7325623f4c7afdf96e6358f22bfe1dd9f
Jim Durand <me@jdurand.com>
2024-07-06 17:27:55 -0400

    nvim: update obsidian keymaps

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -57,6 +57,51 @@ return {
       min_chars = 2,
     },
 
+    -- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
+    -- way then set 'mappings = {}'.
+    mappings = {
+      -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+      ["gf"] = {
+        action = function()
+          return require("obsidian").util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      -- Toggle check-boxes.
+      ["<leader>ch"] = {
+        action = function()
+          return require("obsidian").util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
+      },
+      -- Smart action depending on context, either follow link or toggle checkbox.
+      ["<cr>"] = {
+        action = function()
+          return require("obsidian").util.smart_action()
+        end,
+        opts = { buffer = true, expr = true },
+      },
+      -- Overrides the <leader>ff & <leader>fg mappings for Obsidian functionality
+      ["<leader>ff"] = {
+        action = function()
+          return "<cmd>ObsidianQuickSwitch<CR>"
+        end,
+        opts = { buffer = true, expr = true },
+      },
+      ["<leader>fg"] = {
+        action = function()
+          return "<cmd>ObsidianSearch<CR>"
+        end,
+        opts = { buffer = true, expr = true },
+      },
+      ["<leader>fw"] = {
+        action = function()
+          return "<cmd>ObsidianWorkspace<CR>"
+        end,
+        opts = { buffer = true, expr = true },
+      }
+    },
+
     follow_url_func = function(url)
       -- Open the URL in the default web browser.
       vim.fn.jobstart({ "open", url })  -- Mac OS

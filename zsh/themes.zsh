4903450e78cc51a326fd4a3c95c52f8068eb2a8d
Jim Durand <me@jdurand.com>
2024-07-14 12:59:28 -0400

    zsh: update p10k config (wip)
    hide git stash count

--- zsh/themes.zsh
+++ zsh/themes.zsh
@@ -4,16 +4,17 @@
 
 # Set Oh My Zsh Theme
 # ZSH_THEME="agnoster"
+# ZSH_THEME="robbyrussell"
 ZSH_THEME="powerlevel10k/powerlevel10k"
 
-# Use Powerlevel10k theme when using floaterm
+# Always use Powerlevel10k theme when using floaterm
 if [[ -z "${ZSH_THEME}" ]]; then
   if [[ -n "${FLOATERM}" ]]; then
     ZSH_THEME="powerlevel10k/powerlevel10k"
   fi
 fi
 
-# Optionally load Powerlevel10k config
+# When theme is Powerlevel10k, load Powerlevel10k config
 if [ "$ZSH_THEME" = "powerlevel10k/powerlevel10k" ]; then
   # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
   # Initialization code that may require console input (password prompts, [y/n]
@@ -26,7 +27,7 @@ if [ "$ZSH_THEME" = "powerlevel10k/powerlevel10k" ]; then
   [[ ! -f ~/.zsh/p10k.zsh ]] || source ~/.zsh/p10k.zsh
 fi
 
-# Load Oh My Posh Themes if ZSH_THEME is not set
+# When ZSH_THEME is unset, load Oh My Posh Theme
 # first run: brew install jandedobbeleer/oh-my-posh/oh-my-posh
 # -----------------------------------------------------------------------------
 if [[ -z "${ZSH_THEME}" ]]; then
@@ -38,14 +39,8 @@ if [[ -z "${ZSH_THEME}" ]]; then
     # eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/catppuccin_mocha.omp.json)"
     # eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/powerlevel10k_modern.omp.json)"
 
-    # if [[ -z "${FLOATERM}" ]]; then
-    #   # Use a simpler theme without a right prompt in floaterm, unless Powerlevel10k is configured.
-    #   eval "$(oh-my-posh init zsh --config ~/.zsh/posh-themes/catppuccin.omp.json)"
-    # else
-    #   eval "$(oh-my-posh init zsh --config ~/.zsh/posh-themes/catppuccin_mocha.omp.json)"
-    # fi
-
-    # eval "$(oh-my-posh init zsh --config ~/.zsh/posh-themes/zen.omp.toml)"
-    eval "$(oh-my-posh init zsh --config ~/.zsh/posh-themes/catppuccin_mocha.omp.json)"
+    # eval "$(oh-my-posh init zsh --config ~/.zsh/posh-themes/catppuccin.omp.json)"
+    # eval "$(oh-my-posh init zsh --config ~/.zsh/posh-themes/catppuccin_mocha.omp.json)"
+    eval "$(oh-my-posh init zsh --config ~/.zsh/posh-themes/zen.omp.toml)"
   fi
 fi

cd63c876ad5e1d0f244131575e4a32fa29b33cda
Jim Durand <me@jdurand.com>
2024-10-29 12:25:25 -0400

    setup: add yazi & y wrapper function

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -42,3 +42,13 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     alias meld=/Applications/Meld.app/Contents/MacOS/Meld
   fi
 fi
+
+# wrapper to change directories upon Yazi exit
+function y() {
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+  yazi "$@" --cwd-file="$tmp"
+  if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+    builtin cd -- "$cwd"
+  fi
+  rm -f -- "$tmp"
+}

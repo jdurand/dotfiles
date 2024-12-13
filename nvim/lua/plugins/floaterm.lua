084ab3ce0eb13c9290f9b1964794d7962b6388ba
Jim Durand <me@jdurand.com>
2024-12-12 19:38:36 -0500

    nvim: adjust floaterm lazygit window size

--- nvim/lua/plugins/floaterm.lua
+++ nvim/lua/plugins/floaterm.lua
@@ -4,8 +4,8 @@ return {
   {
     'voldikss/vim-floaterm',
     config = function()
-      nnoremap('<leader>tt', ':FloatermNew<CR>', { desc = '[T]erminal' })
-      nnoremap('<leader>tg', ':FloatermNew lazygit<CR>', { desc = 'Lazy[G]it' })
+      nnoremap('<leader>tt', ':FloatermNew --height=0.8 --width=0.9<CR>', { desc = '[T]erminal' })
+      nnoremap('<leader>tg', ':FloatermNew --height=0.8 --width=0.9 lazygit<CR>', { desc = 'Lazy[G]it' })
       nnoremap('<leader>td', ':FloatermNew! --height=0.9 --width=0.95 --wintype=float --name=gtasks --position=bottom gtasks tasks view --tasklist "Reclaim.ai"<CR>', { desc = 'Google Tasks (TO[D]O)' })
     end,
   }

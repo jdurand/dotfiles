343bacef0f631df396a2365ab06d83c0d71eabec
Jim Durand <me@jdurand.com>
2024-12-15 23:01:20 -0500

    nvim: cleanup lualine config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,55 +1,57 @@
+local neon_theme = require('user/themes/lualine/electric-neon')
+
+local function truncate_branch_name(branch)
+  if not branch or branch == "" then
+    return ''
+  end
+
+  -- Match the branch name to the specified format
+  local _, _, tracker, ticket = string.find(branch, "(.+)/(%D+%-%d+)%-")
+
+  -- If the branch name matches the format, display {tracker}/{project}-{ticket_number}, otherwise display the full branch name
+  if ticket then
+    return string.lower(tracker) .. ': ' ..string.upper(ticket)
+  else
+    return branch
+  end
+end
+
+local function harpoon_component()
+  local harpoon = require('harpoon')
+
+  local mark_count = harpoon:list():length()
+  local mark_list = harpoon:list().items
+
+  if mark_count == 0 then
+    return ''
+  end
+
+  local current_mark = "—"
+  local current_buffer = vim.api.nvim_get_current_buf()
+
+  local mark_index = nil
+
+  for i, item in ipairs(mark_list) do
+    local bufnr = vim.fn.bufnr(item.value, true)
+
+    if bufnr == current_buffer then
+      mark_index = i
+      break
+    end
+  end
+
+  if mark_index ~= nil then
+    current_mark = tostring(mark_index)
+  end
+
+  return string.format("󱡅 %s/%d", current_mark, mark_count)
+end
+
 return {
   {
     'nvim-lualine/lualine.nvim',
     event = 'VeryLazy',
     config = function()
-      local function truncate_branch_name(branch)
-        if not branch or branch == "" then
-          return ''
-        end
-
-        -- Match the branch name to the specified format
-        local _, _, tracker, ticket = string.find(branch, "(.+)/(%D+%-%d+)%-")
-
-        -- If the branch name matches the format, display {tracker}/{project}-{ticket_number}, otherwise display the full branch name
-        if ticket then
-          return string.lower(tracker) .. ': ' ..string.upper(ticket)
-        else
-          return branch
-        end
-      end
-
-      local function harpoon_component()
-        local harpoon = require('harpoon')
-
-        local mark_count = harpoon:list():length()
-        local mark_list = harpoon:list().items
-
-        if mark_count == 0 then
-          return ''
-        end
-
-        local current_mark = "—"
-        local current_buffer = vim.api.nvim_get_current_buf()
-
-        local mark_index = nil
-
-        for i, item in ipairs(mark_list) do
-          local bufnr = vim.fn.bufnr(item.value, true)
-
-          if bufnr == current_buffer then
-            mark_index = i
-            break
-          end
-        end
-
-        if mark_index ~= nil then
-          current_mark = tostring(mark_index)
-        end
-
-        return string.format("󱡅 %s/%d", current_mark, mark_count)
-      end
-
       require('lualine').setup({
         options = {
           -- theme = 'auto',
@@ -80,9 +82,5 @@ return {
         },
       })
     end,
-    dependencies = {
-      -- 'catppuccin/vim'
-      'folke/tokyonight.nvim'
-    }
   },
 }

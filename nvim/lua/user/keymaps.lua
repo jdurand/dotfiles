8e8420f22ac40f71997f1b0bb9beee96207a6b84
Jim Durand <me@jdurand.com>
2025-01-04 14:10:41 -0500

    nvim: enable mouse support

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -1,5 +1,5 @@
 local keymaps = require('user.keymaps.bind')
-local nnoremap, nmap = keymaps.nnoremap, keymaps.nmap
+local nnoremap, nmap, vnoremap = keymaps.nnoremap, keymaps.nmap, keymaps.vnoremap
 
 -- Define the ZenMode keybind here to prevent conflicts
 -- with `config` block options in the plugin file
@@ -15,3 +15,6 @@ end, { desc = 'Open in [Z]en Mode' })
 nmap('<leader>yf', function()
   vim.fn.setreg('*', vim.fn.expand('%'))
 end, { desc = 'Copy [f]ile path to clipboard' })
+
+-- Yank selected text on left mouse release
+vnoremap('<LeftRelease>', '"*ygv<escape>', { desc = 'Yank on mouse selection' })
--- nvim/lua/user/keymaps/bind.lua
+++ nvim/lua/user/keymaps/bind.lua
@@ -1,18 +1,19 @@
 local M = {}
 
-local function bind(op, outer_opts)
+local function bind(mode, outer_opts)
   outer_opts = vim.tbl_extend("force", { noremap = true, silent = true }, outer_opts or {})
 
-  return function(lhs, rhs, opts, docs)
+  return function(keys, action, opts, docs)
     opts = vim.tbl_extend("force", outer_opts, opts or {})
     docs = docs or {}
 
-    vim.keymap.set(op, lhs, rhs, opts)
+    vim.keymap.set(mode, keys, action, opts)
   end
 end
 
 M.map = bind("")
 M.nmap = bind("n", { noremap = false })
+M.vmap = bind("v", { noremap = false })
 M.tmap = bind("t", { noremap = false })
 M.nnoremap = bind("n")
 M.vnoremap = bind("v")

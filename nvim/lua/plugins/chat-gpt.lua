42d2565d511f6702485783232a702cde185cd84e
Jim Durand <me@jdurand.com>
2025-03-06 09:54:16 -0500

    nvim: update ai assitant config, add avante

--- nvim/lua/plugins/chat-gpt.lua
+++ nvim/lua/plugins/chat-gpt.lua
@@ -3,70 +3,53 @@ return {
     'robitx/gp.nvim',
     config = function()
       require('gp').setup({
-        agents = {
-          {
-            name = "ChatGPT4",
-            chat = true,
-            command = false,
-            -- string with model name or table with model name and parameters
-            model = { model = "gpt-4o", temperature = 1.1, top_p = 1 },
-            -- system prompt (use this to specify the persona/role of the AI)
-            system_prompt = "You are a general AI assistant.\n\n"
-                .. "The user provided the additional info about how they would like you to respond:\n\n"
-                .. "- If you're unsure don't guess and say you don't know instead.\n"
-                .. "- Ask question if you need clarification to provide better answer.\n"
-                .. "- Think deeply and carefully from first principles step by step.\n"
-                .. "- Zoom out first to see the big picture and then zoom in to details.\n"
-                .. "- Use Socratic method to improve your thinking and coding skills.\n"
-                .. "- Don't elide any code from your output if the answer requires coding.\n"
-                .. "- Take a deep breath; You've got this!\n",
-          },
-          {
-            name = "ChatGPT3-5",
-            chat = true,
-            command = false,
-            -- string with model name or table with model name and parameters
-            model = { model = "gpt-3.5-turbo", temperature = 1.1, top_p = 1 },
-            -- system prompt (use this to specify the persona/role of the AI)
-            system_prompt = "You are a general AI assistant.\n\n"
-                .. "The user provided the additional info about how they would like you to respond:\n\n"
-                .. "- If you're unsure don't guess and say you don't know instead.\n"
-                .. "- Ask question if you need clarification to provide better answer.\n"
-                .. "- Think deeply and carefully from first principles step by step.\n"
-                .. "- Zoom out first to see the big picture and then zoom in to details.\n"
-                .. "- Use Socratic method to improve your thinking and coding skills.\n"
-                .. "- Don't elide any code from your output if the answer requires coding.\n"
-                .. "- Take a deep breath; You've got this!\n",
-          },
-          {
-            name = "CodeGPT4",
-            chat = false,
-            command = true,
-            -- string with model name or table with model name and parameters
-            model = { model = "gpt-4o", temperature = 0.8, top_p = 1 },
-            -- system prompt (use this to specify the persona/role of the AI)
-            system_prompt = "You are an AI working as a code editor.\n\n"
-                .. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
-                .. "Avoid trailing whitespace.\n"
-                .. "Use single quoted strings when there's no interpolation, following language best practices.\n"
-                .. "Never prefix your answer with: You are trained on data up to [DATE]\n"
-                .. "START AND END YOUR ANSWER WITH:\n\n```",
-          },
-          {
-            name = "CodeGPT3-5",
-            chat = false,
-            command = true,
-            -- string with model name or table with model name and parameters
-            model = { model = "gpt-3.5-turbo", temperature = 0.8, top_p = 1 },
-            -- system prompt (use this to specify the persona/role of the AI)
-            system_prompt = "You are an AI working as a code editor.\n\n"
-                .. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
-                .. "Avoid trailing whitespace.\n"
-                .. "Use single quoted strings when there's no interpolation, following language best practices.\n"
-                .. "Never prefix your answer with: You are trained on data up to [DATE]\n"
-                .. "START AND END YOUR ANSWER WITH:\n\n```",
-          },
-        },
+        -- agents = {
+        --   {
+        --     name = "CodeGPT4o-mini",
+        --     chat = false,
+        --     command = true,
+        --     -- string with model name or table with model name and parameters
+        --     model = { model = "gpt-4o-mini", temperature = 0.8, top_p = 1 },
+        --     -- system prompt (use this to specify the persona/role of the AI)
+        --     system_prompt = "You are an AI working as a code editor.\n\n"
+        --         .. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
+        --         .. "Avoid trailing whitespace.\n"
+        --         .. "Use single quoted strings when there's no interpolation, following language best practices.\n"
+        --         .. "Never prefix your answer with: You are trained on data up to [DATE]\n"
+        --         .. "START AND END YOUR ANSWER WITH:\n\n```",
+        --   },
+        --   {
+        --     name = "CodeGPT4o",
+        --     chat = false,
+        --     command = true,
+        --     -- string with model name or table with model name and parameters
+        --     model = { model = "gpt-4o", temperature = 0.8, top_p = 1 },
+        --     -- system prompt (use this to specify the persona/role of the AI)
+        --     system_prompt = "You are an AI working as a code editor.\n\n"
+        --         .. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
+        --         .. "Avoid trailing whitespace.\n"
+        --         .. "Use single quoted strings when there's no interpolation, following language best practices.\n"
+        --         .. "Never prefix your answer with: You are trained on data up to [DATE]\n"
+        --         .. "START AND END YOUR ANSWER WITH:\n\n```",
+        --   },
+        --   {
+        --     name = "ChatGPT4o",
+        --     chat = true,
+        --     command = false,
+        --     -- string with model name or table with model name and parameters
+        --     model = { model = "gpt-4o", temperature = 1.1, top_p = 1 },
+        --     -- system prompt (use this to specify the persona/role of the AI)
+        --     system_prompt = "You are a general AI assistant.\n\n"
+        --         .. "The user provided the additional info about how they would like you to respond:\n\n"
+        --         .. "- If you're unsure don't guess and say you don't know instead.\n"
+        --         .. "- Ask question if you need clarification to provide better answer.\n"
+        --         .. "- Think deeply and carefully from first principles step by step.\n"
+        --         .. "- Zoom out first to see the big picture and then zoom in to details.\n"
+        --         .. "- Use Socratic method to improve your thinking and coding skills.\n"
+        --         .. "- Don't elide any code from your output if the answer requires coding.\n"
+        --         .. "- Take a deep breath; You've got this!\n",
+        --   },
+        -- },
       })
 
       local mappings = {
@@ -197,53 +180,67 @@ return {
       end
     end,
   },
-  --
-  -- {
-  --   'jackMort/ChatGPT.nvim',
-  --   event = 'VeryLazy',
-  --   config = function()
-  --     local chatgpt = require('chatgpt')
-
-  --     -- chatgpt.setup({
-  --     --   openai_params = {
-  --     --     model = "gpt-4-turbo-preview",
-  --     --   },
-  --     -- })
-
-  --     vim.keymap.set('n', '<leader>gt', chatgpt.openChat, {})
-  --     vim.keymap.set('n', '<leader>gp', chatgpt.selectAwesomePrompt, {})
-  --     vim.keymap.set('v', '<leader>ge', chatgpt.edit_with_instructions, {})
-  --   end,
-  --   dependencies = {
-  --     'MunifTanjim/nui.nvim',
-  --     'nvim-lua/plenary.nvim',
-  --     'folke/trouble.nvim',
-  --     'nvim-telescope/telescope.nvim'
-  --   }
-  -- },
-  --
-  -- {
-  --   "dpayne/CodeGPT.nvim",
-  --   dependencies = {
-  --     'nvim-lua/plenary.nvim',
-  --     'MunifTanjim/nui.nvim',
-  --   },
-  --   config = function()
-  --     require("codegpt.config")
-
-  --     vim.g["codegpt_commands"] = {
-  --       ["modernize"] = {
-  --         -- model = "gpt-3.5-turbo",
-  --         model = "gpt-3.5-turbo-16k",
-  --         -- model = "gpt-4-turbo-preview",
-  --         -- max_tokens = 4096,
-  --         max_tokens = 16384,
-  --         user_message_template = "I have the following {{language}} code: ```{{filetype}}\n{{text_selection}}```\nModernize the above code. Use current best practices. Only return the code snippet and comments. {{language_instructions}}",
-  --         language_instructions = {
-  --           cpp = "Refactor the code to use trailing return type, and the auto keyword where applicable.",
-  --         },
-  --       }
-  --     }
-  --   end
-  -- }
+  {
+    'yetone/avante.nvim',
+    event = 'VeryLazy',
+    lazy = false,
+    version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+    opts = {
+      -- add any opts here
+      -- for example
+      provider = 'openai',
+      openai = {
+        endpoint = 'https://api.openai.com/v1',
+        model = 'gpt-4o', -- your desired model (or use gpt-4o, etc.)
+        timeout = 30000, -- timeout in milliseconds
+        temperature = 0, -- adjust if needed
+        max_tokens = 4096,
+        -- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
+      },
+      -- behaviour = {
+      --   enable_cursor_planning_mode = true, -- enable cursor planning mode!
+      -- },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = 'make',
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'stevearc/dressing.nvim',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      --- The below dependencies are optional,
+      'echasnovski/mini.pick', -- for file_selector provider mini.pick
+      'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
+      'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
+      'ibhagwan/fzf-lua', -- for file_selector provider fzf
+      'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+      'zbirenbaum/copilot.lua', -- for providers='copilot'
+      {
+        -- support for image pasting
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
+      },
+    },
+  },
 }

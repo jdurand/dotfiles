1a00d81dbeff9d01e19c7ee9db8582e817150ee5
Jim Durand <me@jdurand.com>
2025-08-28 09:38:56 -0400

    tmux: do not automatically launch tmux from the vscode terminal

--- fish/startup.fish
+++ fish/startup.fish
@@ -1,4 +1,4 @@
-if not test "$TERM_PROGRAM" = "WarpTerminal"
+if not test "$TERM_PROGRAM" = "WarpTerminal"; and not test "$TERM_PROGRAM" = "vscode"
   echo "New Fish session... Starting Tmux now..."
   tmux attach-session -t main || tmux new-session -s main
 end

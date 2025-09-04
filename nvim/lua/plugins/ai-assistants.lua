72d5a103d6323acf790585087b5655f377889d42
Jim Durand <me@jdurand.com>
2025-09-03 21:39:24 -0400

    nvim: add opencode dependency and keybinds

--- nvim/lua/plugins/ai-assistants.lua
+++ nvim/lua/plugins/ai-assistants.lua
@@ -383,7 +383,7 @@ return {
           local buffer = vim.api.nvim_get_current_buf()
           local name = vim.api.nvim_buf_get_name(buffer)
 
-          if name:match('claude') then
+          if name:match('claude|opencode') then
             -- -- Switch to normal mode when pressing Escape in terminal mode
             -- tnoremap('<Esc>', '<C-\\><C-n>', { buffer = buffer })
             --
@@ -421,6 +421,31 @@ return {
       -- -- Diff management
       -- { '<leader>aa', '<cmd>ClaudeCodeDiffAccept<cr>', desc = 'claude: accept diff' },
       -- { '<leader>ad', '<cmd>ClaudeCodeDiffDeny<cr>', desc = 'claude: deny diff' },
+    }
+  },
+  {
+    'NickvanDyke/opencode.nvim',
+    dependencies = {
+      -- Recommended for better prompt input, and required to use opencode.nvim's embedded terminal — otherwise optional
+      { 'folke/snacks.nvim', opts = { input = { enabled = true } } },
+    },
+    ---@type opencode.Opts
+    opts = {
+      -- Your configuration, if any — see lua/opencode/config.lua
+    },
+    keys = {
+      -- Recommended keymaps
+      { '<leader>Ac', function() require('opencode').ask() end, desc = 'Ask opencode', },
+      { '<leader>As', function() require('opencode').ask('@cursor: ') end, desc = 'Ask opencode about this', mode = 'n', },
+      { '<leader>As', function() require('opencode').ask('@selection: ') end, desc = 'Ask opencode about selection', mode = 'v', },
+      { '<leader>Aa', function() require('opencode').toggle() end, desc = 'Toggle embedded opencode', mode = 'n' },
+      { '<leader>An', function() require('opencode').command('session_new') end, desc = 'New session', },
+      { '<leader>Ay', function() require('opencode').command('messages_copy') end, desc = 'Copy last message', },
+      { '<S-C-u>',    function() require('opencode').command('messages_half_page_up') end, desc = 'Scroll messages up', },
+      { '<S-C-d>',    function() require('opencode').command('messages_half_page_down') end, desc = 'Scroll messages down', },
+      { '<leader>Ap', function() require('opencode').select_prompt() end, desc = 'Select prompt', mode = { 'n', 'v', }, },
+      -- Example: keymap for custom prompt
+      { '<leader>Ae', function() require('opencode').prompt("Explain @cursor and its context") end, desc = "Explain code near cursor", },
     },
   }
 }

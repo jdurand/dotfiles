27966dda6bfcad2934ba1a045522faa5a17c607b
Jim Durand <me@jdurand.com>
2026-09-17 00:34:15 -0400

    update font/elments size

--- hypr/monitors.lua
+++ hypr/monitors.lua
@@ -4,7 +4,7 @@
 hl.env("GDK_SCALE", "2")
 
 -- BenQ PD3225U above the Acer PM161QT portable touchscreen.
-hl.monitor({ output = "DP-3", mode = "3840x2160@60", position = "0x0", scale = 1.5 })
+hl.monitor({ output = "DP-3", mode = "3840x2160@60", position = "0x0", scale = 1.75 })
 hl.monitor({ output = "HDMI-A-1", mode = "1920x1080@60", position = "auto-center-down", scale = 1.25 })
 hl.monitor({ output = "", mode = "preferred", position = "auto", scale = "auto" })
 

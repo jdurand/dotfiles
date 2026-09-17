30f33efd9715e361a84c6c8c1f92aafd7b7d954f
Jim Durand <me@jdurand.com>
2026-09-17 00:03:22 -0400

    add tap toggle dictation

--- hypr/bindings.lua
+++ hypr/bindings.lua
@@ -60,8 +60,20 @@ o.bind("SUPER + SHIFT + G", "Move window to workspace 5", hl.dsp.window.move({ w
 o.bind("SUPER + SHIFT + B", "Move window to workspace 6", hl.dsp.window.move({ workspace = "6" }))
 
 o.bind("SUPER + SHIFT + CTRL + G", "Google Chat", { webapp = "https://chat.google.com/", focus = true })
-o.bind("ISO_Next_Group", "Start dictation (push-to-talk)", "voxtype record start")
-o.bind("ISO_Next_Group", "Stop dictation (push-to-talk)", "voxtype record stop", { release = true })
+local globe_held = false
+o.bind("ISO_Next_Group", "Dictation", function() end)
+o.bind("ISO_Next_Group", "Start dictation (push-to-talk)", function()
+  globe_held = true
+  hl.dispatch(hl.dsp.exec_cmd("voxtype record start"))
+end, { long_press = true })
+o.bind("ISO_Next_Group", "Stop or toggle dictation", function()
+  if globe_held then
+    globe_held = false
+    hl.dispatch(hl.dsp.exec_cmd("voxtype record stop"))
+  else
+    hl.dispatch(hl.dsp.exec_cmd("voxtype record toggle"))
+  end
+end, { release = true })
 o.bind("SUPER + EQUAL", "Zoom in", "hyprctl -q keyword cursor:zoom_factor $(hyprctl getoption cursor:zoom_factor | awk '/^float.*/ {print $2 * 1.1}')")
 o.bind("SUPER + MINUS", "Zoom out", "hyprctl -q keyword cursor:zoom_factor $(hyprctl getoption cursor:zoom_factor | awk '/^float.*/ {print $2 * 0.9}')")
 o.bind("SUPER + KP_ADD", "Zoom in (numpad)", "hyprctl -q keyword cursor:zoom_factor $(hyprctl getoption cursor:zoom_factor | awk '/^float.*/ {print $2 * 1.1}')")
